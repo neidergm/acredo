2a9850f138422281a795eef35ca9d875cc664ba9
Developer_Neider_CURN <neider.galofre@curn.edu.co>
2022-11-17 16:23:39 -0500

    Alert (temporal), Modal and loader

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,47 +1,96 @@
 import { useState, useEffect } from 'react';
-import { Button, Modal as ModalB, ModalHeader, ModalBody, ModalFooter, ModalProps, ButtonProps } from 'reactstrap';
+import { Button, Modal as ModalB, ModalHeader, ModalBody, ModalFooter as ModalFooterB, ModalProps, ButtonProps } from 'reactstrap';
 
 export interface I_ModalContentProps {
-    title?: JSX.Element | JSX.Element[] | any;
-    buttons: ButtonProps[];
     children: JSX.Element | JSX.Element[] | string;
+    closeButton?: ButtonProps;
+    submitButton?: ButtonProps;
+    title?: JSX.Element | JSX.Element[] | any;
+    onClosed?: () => void;
+    isOpen?: boolean;
 }
 
-interface I_ModalProps {
-    toggleCallback: () => void;
-}
+export const ModalFooter = ({
+    btn1,
+    btn2,
+    action
+}: {
+    btn1?: ButtonProps,
+    btn2?: ButtonProps,
+    action: Function
+}) => (btn1 || btn2) ?
+    <ModalFooterB style={{ border: 0, justifyContent: btn1 && btn2 ? "space-between" : "center" }}>
+        {btn1 &&
+            <Button color='primary' {...btn1}
+                onClick={() => { btn1.onClick?.(action as any) || action() }}
+            >
+                {btn1.value || "Cancelar"}
+            </Button>
+        }
+        {btn2 &&
+            <Button color='primary' {...btn2}
+                onClick={() => {
+                    if (!!(btn2.onClick)) return btn2.onClick(action as any);
+                    action()
+                }}
+            >
+                {btn2.value || "Ok"}
+            </Button>
+        }
+    </ModalFooterB> : null
 
 const Modal = ({
-    toggleCallback,
     title,
     children,
-    buttons,
+    submitButton,
+    closeButton,
+    onClosed,
     isOpen
-}: I_ModalProps & ModalProps & I_ModalContentProps) => {
+}: ModalProps & I_ModalContentProps) => {
     const [showModal, setShowModal] = useState(isOpen);
 
     useEffect(() => {
         setShowModal(isOpen);
     }, [isOpen])
 
-    const toggle = () => setShowModal(!showModal);
+    const toggle = (action?: boolean) => {
+        action !== undefined ? setShowModal(action) : setShowModal(!showModal);
+    };
 
     return (
         <ModalB
+            modalTranssition
+            contentClassName='p-md-2 p-xl-3 border-0'
             style={{ border: 0 }}
             isOpen={showModal}
-            toggle={toggle}
+            toggle={() => toggle()}
             centered
             fullscreen="sm"
-            onClosed={toggleCallback}
+            onClosed={onClosed}
         >
-            <ModalHeader toggle={toggle} style={{ border: 0 }}>{title}</ModalHeader>
+            {title && <ModalHeader toggle={() => toggle()} style={{ border: 0 }}>{title}</ModalHeader>}
             <ModalBody>{children}</ModalBody>
-            <ModalFooter style={{ border: 0, justifyContent: "space-between" }}>
-                {buttons?.map(({ value, ...button }, i) => <Button {...button} key={i}>{value}</Button>)}
-            </ModalFooter>
+            <ModalFooter btn1={closeButton} btn2={submitButton} action={toggle} />
         </ModalB>
     );
 }
 
 export default Modal;
+
+
+/**
+ * Usage example
+ * 
+ * setModalData({
+ *   title: "hola",
+ *   closeButton: { color: "danger" },
+ *   submitButton: { color: "success", onClick: successAction as any },
+ *   children: "lorem",
+ * })
+ * 
+ * const successAction = (toggle: Function) => {
+ *      console.log("ok")
+ *      setModalData({ children: "Cargando" })
+ *      toggle(true)
+ *  }
+ */
