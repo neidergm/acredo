1c7cfaafde14d16445d22fdc482b6e196f92817e
Developer_Neider_CURN <neider.galofre@curn.edu.co>
2022-11-23 16:37:28 -0500

    endpoints fixed

--- src/services/endPointsService.ts
+++ src/services/endPointsService.ts
@@ -4,36 +4,36 @@ import { baseUrl } from "./constantsService";
  * [GET]
  * /conv
  */
-export const CONVOCATORIES_LIST = `${baseUrl}/conv`;
+export const CONVOCATORIES_LIST = `conv`;
 
 /**
  * [GET]
  * /conv/{id_conv}
  */
-export const CONVOCATORY_DETAILS = `${baseUrl}/conv/`;
+export const CONVOCATORY_DETAILS = `conv/`;
 
 /**
  * [GET]
  * /cond/all/{id_conv}
  */
-export const CONDITIONS_BY_CONVOCATORY = `${baseUrl}/cond/all/`;
+export const CONDITIONS_BY_CONVOCATORY = `cond/all/`;
 
 /**
  * [GET]
  * /cond/detalle/{id_cond}
  */
-export const CONDITION_DETAILS = `${baseUrl}/cond/detalle/`;
+export const CONDITION_DETAILS = `cond/detalle/`;
 
 /**
  * [GET]
  * /resp/all/{id_fcamp}
  */
-export const FORM_ANSWERS = `${baseUrl}/resp/all/`;
+export const FORM_ANSWERS = `resp/all/`;
 
 /**
  * [GET]
  * /form/campos/{fields}
  */
-export const FORM_FIELDS = `${baseUrl}/form/campos/`;
+export const FORM_FIELDS = `form/campos/`;
 
 
