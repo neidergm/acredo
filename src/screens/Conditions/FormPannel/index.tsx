bccb1ad68b89c56c3a1a3012d52892d630232436
Neider Galofre Morales <desarrollador2@unicurn.edu.co>
2023-10-20 14:24:04 -0500

    [FIX]: Bug when try edit an attachment

--- src/screens/Conditions/FormPannel/index.tsx
+++ src/screens/Conditions/FormPannel/index.tsx
@@ -112,6 +112,8 @@ const FormPannel = ({
     const submitAll = (data: any, formItem: T_Form, callback?: () => void) => {
         const method = formItem.est_resp === 1 ? "PUT" : "POST";
         data = getDifferenceBetweenData(formItem.defaultValues, data);
+        // eslint-disable-next-line no-debugger
+        debugger;
         if (!Object.keys(data).length) return toast.error("No hay cambios para guardar", { position: "top-right", icon: <i className='text-warning'><ExclamationCircleFill /> </i> })
         openLoader("Guardando datos");
         const keysOnField = ["id_campo"];
@@ -167,8 +169,11 @@ const FormPannel = ({
             if (item.tipo_form !== 0) {
                 multiplesAnswers = answers.reduce((p, c) => {
                     if (c.json_campo.name === "tipo_anexo") {
-                    (c.json_campo.validations as any).disabled = true;
-                    }
+                    // (c.json_campo.validations as any).disabled = true;
+                    // (c.json_campo.validations as any) = { disabled: true };
+                    (c.json_campo as any).disabled = true;
+                    (c.json_campo.children as any) = null;
+                }
                     if (c.nomb_anexo) isAttachmentsTable = true;
                     p[c.grupo_resp] = [...(p[c.grupo_resp] || []), c];
                     return { ...p }
